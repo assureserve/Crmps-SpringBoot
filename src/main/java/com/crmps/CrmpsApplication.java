689b2183cae0f996ca5bb565a86955e761ef10fc
Sathish Velayudam <sathishv@gmail.com>
2017-04-18 19:57:18 -0400

    Converted to war

--- src/main/java/com/crmps/CrmpsApplication.java
+++ src/main/java/com/crmps/CrmpsApplication.java
@@ -8,11 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class CrmpsApplication {
+public class CrmpsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrmpsApplication.class, args);
